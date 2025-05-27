3049c2f3a683b6631c44f0423714b8c316a0b4f3
Tignaman88 <alessioscarpello2809@gmail.com>
2025-05-27 18:18:52 +0100

    Branches Added
    Check if user wants to deposit or withdraw

--- SimpleBankingApp/src/Main.java
+++ SimpleBankingApp/src/Main.java
@@ -5,28 +5,62 @@ public class Main {
     public static void main(String[] args) {
 
         //Create an object
-        BankUser Alessio = new BankUser("Alessio", "Scarpello", 2458.24);
+        BankUser AlessioScarpello = new BankUser("Alessio", "Scarpello", 2458.24);
 
         // Create input variable to be used to deposit or withdraw
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! Please enter D to deposit, or W to withdraw money from your account: ");
-        String userInput = scanner.nextLine();
-        double moneyInput = Double.parseDouble(userInput);
-        Alessio.deposit(Alessio.accountMoney, moneyInput);
-        System.out.println("Hello " + Alessio.getFirstName() + ", you have now " +
-                "£" + Alessio.accountMoney + " into your account");
-        System.out.println("Hello! Please enter the amount you'd like to withdraw: ");
-        while (true) {
-            userInput = scanner.nextLine();
-            moneyInput = Double.parseDouble(userInput);
-            if (Alessio.withdraw(Alessio.accountMoney, moneyInput)) {
-                System.out.println("You cannot withdraw more than what you have, please try again: ");
-            } else {
-                System.out.println("Hello " + Alessio.getFirstName() + ", you have " +
-                        "£" + Alessio.accountMoney + " remaining into your account.");
-                break;
+        String moneyInput;
+        double moneyToDeposit;
+        double moneyToWithdraw;
+        boolean isDepositOrWithdraw = true;
+        boolean isNumeric = true;
+        while (isDepositOrWithdraw) {
+            String userInput = scanner.nextLine();
+            if (!userInput.equalsIgnoreCase("d") && !userInput.equalsIgnoreCase("w")) {
+                System.out.println("You have not selected the right option, please try again: ");
+            } else if (userInput.equalsIgnoreCase("d")) {
+                System.out.println("Great! How much you'd like to deposit? ");
+
+                // Check if money input by user is a number
+                while (isNumeric) {
+                    moneyInput = scanner.nextLine();
+                    if (!isNumeric(moneyInput)) {
+                        System.out.println("You have not selected a valid number, please try again: ");
+                    } else {
+                        moneyToDeposit = Double.parseDouble(moneyInput);
+                        AlessioScarpello.deposit(AlessioScarpello.accountMoney, moneyToDeposit);
+                        System.out.println("Hello " + AlessioScarpello.getFirstName() + ", here's your updated balance:" +
+                                "£" + AlessioScarpello.getAccountMoney());
+                        isNumeric = false;
+                    }
+                }
+
+                isDepositOrWithdraw = false;
+            } else if (userInput.equalsIgnoreCase("w")) {
+                System.out.println("Great! How much you'd like to withdraw?");
+
+                // Check if money input by user is a number
+                while (isNumeric) {
+                    moneyInput = scanner.nextLine();
+                    if (!isNumeric(moneyInput)) {
+                        System.out.println("You have not selected a valid number, please try again: ");
+                    } else {
+                        moneyToWithdraw = Double.parseDouble(moneyInput);
+                        AlessioScarpello.withdraw(AlessioScarpello.accountMoney, moneyToWithdraw);
+                        System.out.println("Hello " + AlessioScarpello.getFirstName() + ", here's your updated balance: " +
+                                "£" + AlessioScarpello.getAccountMoney());
+                        isNumeric = false;
+                    }
+                }
+
+                isDepositOrWithdraw = false;
             }
+
+
         }
+
+
 
 
     }
