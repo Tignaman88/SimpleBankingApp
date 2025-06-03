8771d61116f6698f3d321f01381e8715cc629585
Tignaman88 <alessioscarpello2809@gmail.com>
2025-06-03 17:33:18 +0100

    Last Condition Added
    Check if match has been found

--- SimpleBankingApp/src/Main.java
+++ SimpleBankingApp/src/Main.java
@@ -74,7 +74,7 @@ public class Main {
                                 isUserInArrayList = false;
                             } else {
                                 bankUserFullName = bankUserFirstName + bankUserLastName;
-                                
+
                             }
                     }
 
